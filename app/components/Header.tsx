e16fd86e532e743b317712fedceff219f8c7c707
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-08-04 12:57:16 -0700

    Removed src folder and set basic component for Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="font-poppins text-3xl font-bold">FutStats</h1>
+    </header>
+  );
+}
