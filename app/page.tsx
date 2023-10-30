e9415e5b611e509d902d830741844a6c6d340c4a
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-30 10:52:24 -0700

    Created standings and upcoming routes

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,15 @@ import Highlights from "./components/Highlights";
 export default function Home() {
   const callAPI = async () => {
     try {
-      const res = await fetch(`https://api.football-data.org/v4/matches`, {
-        mode: "no-cors",
-      });
+      const res = await fetch(
+        "https://api.football-data.org/v4/teams/86/matches?status=SCHEDULED",
+        {
+          mode: "no-cors",
+          headers: {
+            "X-Auth-Token": "84a721b3da6a410298a1fb584f79a8d6",
+          },
+        }
+      );
       const data = await res.json();
       console.log(data);
     } catch (err) {
