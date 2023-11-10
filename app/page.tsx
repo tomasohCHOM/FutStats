1cb0907e5a81bfb113913b8fb3e83f7d63e22e78
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-11-10 15:59:38 -0800

    Moved callAPI outside UI component

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,30 @@
 
 import Highlights from "./components/Highlights";
 
-export default function Home() {
-  const callAPI = async () => {
-    try {
-      const res = await fetch(
-        "https://api.football-data.org/v4/teams/86/matches?status=SCHEDULED",
-        {
-          mode: "no-cors",
-          headers: {
-            "X-Auth-Token": process.env.NEXT_PUBLIC_API_TOKEN ?? "",
-          },
-        }
-      );
-      const data = await res.json();
-      console.log(data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+const callAPI = async () => {
+  try {
+    const res = await fetch(
+      "https://api.football-data.org/v4/teams/86/matches?status=SCHEDULED",
+      {
+        mode: "no-cors",
+        headers: {
+          // "Access-Control-Allow-Origin": "*",
+          // "Access-Control-Allow-Credentials": true,
+          "Content-Type": "applications/json",
+          "X-Auth-Token": "84a721b3da6a410298a1fb584f79a8d6",
+        },
+      }
+    );
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
+export default function Home() {
   return (
     <main className="grid grid-cols-4">
       <Highlights></Highlights>
