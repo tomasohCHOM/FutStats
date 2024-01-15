fdf878360568388f852d834b4a0ea795f706519f
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-15 14:49:16 -0800

    Added remaining routes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,18 +1,35 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Navbar() {
   return (
     <header className="flex items-center justify-between px-6 py-3 shadow-md">
       <section>
-        <h1 className="font-poppins text-3xl font-bold text-gray-10 cursor-pointer">
-          FutStats
-        </h1>
+        <Link href="/">
+          <h1 className="font-poppins text-3xl font-bold text-gray-10 cursor-pointer">
+            FutStats
+          </h1>
+        </Link>
       </section>
-      <section className="font-poppins font-normal text-xl flex items-center justify-between [&>div]:mx-3 [&>div]:cursor-pointer">
-        <div className="hover:text-contrast-200 transition-all">Highlights</div>
-        <div className="hover:text-contrast-200 transition-all">Upcoming</div>
-        <div className="hover:text-contrast-200 transition-all">Standings</div>
-        <div className="hover:text-contrast-200 transition-all">News</div>
+      <section className="font-poppins font-normal text-xl flex items-center justify-between gap-4 [&>span]:cursor-pointer">
+        <Link href="/highlights">
+          <span className="hover:text-contrast-200 transition-all">
+            Highlights
+          </span>
+        </Link>
+        <Link href="/upcoming">
+          <span className="hover:text-contrast-200 transition-all">
+            Upcoming
+          </span>
+        </Link>
+        <Link href="/standings">
+          <span className="hover:text-contrast-200 transition-all">
+            Standings
+          </span>
+        </Link>
+        <Link href="/news">
+          <span className="hover:text-contrast-200 transition-all">News</span>
+        </Link>
       </section>
     </header>
   );
