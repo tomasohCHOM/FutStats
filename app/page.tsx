3eeb2c082f679c9066a3d841ddf226584feb9bf2
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-01-15 15:53:07 -0800

    Accept parameters in fetch function

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Highlights from "./components/Highlights";
 import { getFootballData } from "./utils/fetch";
 
 export default async function Home() {
-  const data = await getFootballData();
+  const data = await getFootballData("competitions/PL");
+  console.log(data);
 
   return (
     <main className="grid grid-cols-4">
--- app/utils/fetch.ts
+++ app/utils/fetch.ts
@@ -1,5 +1,5 @@
-export async function getFootballData() {
-  const res = await fetch("https://api.football-data.org/v4/areas/2077", {
+export async function getFootballData(endpoint: string) {
+  const res = await fetch(`https://api.football-data.org/v4/${endpoint}`, {
     headers: {
       "X-Auth-Token": process.env.NEXT_PUBLIC_API_TOKEN || "",
     },
