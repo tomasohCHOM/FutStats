78fb045cad173230a1491bc4522f79ff4dfcd300
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-02-01 12:47:13 -0800

    Iterated through seasons returned json

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,30 @@
 import Highlights from "./components/Highlights";
 import { getFootballData } from "./utils/fetch";
 
-export default async function Home() {
-  const data = await getFootballData("competitions/PL");
-  console.log(data);
+interface Season {
+  seasons: {
+    id: number;
+    startDate: string;
+    endDate: string;
+    currentMatchDay: string;
+    winner: {
+      id: number;
+      name: string;
+      crest: string;
+      website: string;
+    };
+  }[];
+}
 
+export default async function Home() {
+  const data: Season = await getFootballData("competitions/PL");
   return (
     <main className="grid grid-cols-4">
-      <Highlights></Highlights>
+      {data.seasons.map((season) => {
+        if (!season.winner) return <div>Season not played yet</div>;
+        return <div>{season.winner.name}</div>;
+      })}
+      {/* <Highlights></Highlights> */}
     </main>
   );
 }
