12d11e36ee64a217f1ca0ff4f34709e3682b6999
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-07-05 13:48:44 -0700

    Add standings

--- app/standings/page.tsx
+++ app/standings/page.tsx
@@ -1,5 +1,62 @@
 import React from "react";
+import { getFootballData } from "../utils/fetch";
+import Image from "next/image";
 
-export default function Standings() {
-  return <div>Standings</div>;
+export default async function Standings() {
+  const premierLeagueStandings = await getFootballData(
+    "competitions/PL/standings"
+  );
+  const primeraDivisionStandings = await getFootballData(
+    "competitions/PD/standings"
+  );
+
+  return (
+    <section>
+      <h1 className="text-3xl font-semibold">Standings</h1>
+      <div className="flex gap-4">
+        <div className="flex flex-col">
+          <h2 className="text-xl font-medium">Premier League (England)</h2>
+          <div className="grid">
+            {premierLeagueStandings.standings[0].table.map(
+              (positionEntry: any, i: number) => {
+                return (
+                  <span key={"entry" + i}>
+                    {positionEntry.position} - {positionEntry.team.name}{" "}
+                    <img
+                      src={positionEntry.team.crest}
+                      alt={positionEntry.team.name + " crest"}
+                      width={100}
+                      height={100}
+                      className="inline w-4"
+                    />
+                  </span>
+                );
+              }
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="text-xl font-medium">Primera Division (Spain)</h2>
+          <div className="grid">
+            {primeraDivisionStandings.standings[0].table.map(
+              (positionEntry: any, i: number) => {
+                return (
+                  <span key={"entry" + i}>
+                    {positionEntry.position} - {positionEntry.team.name}{" "}
+                    <img
+                      src={positionEntry.team.crest}
+                      alt={positionEntry.team.name + " crest"}
+                      width={100}
+                      height={100}
+                      className="inline w-4"
+                    />
+                  </span>
+                );
+              }
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
