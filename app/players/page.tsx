7887740c709955a9d265a7096fab2563f3b62a9c
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-07-12 11:56:42 -0700

    Create players/ page

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getFootballData } from "@/lib/fetch";
 import { nationalLeagues } from "@/lib/mappings";
+import { ScrollArea } from "@/components/scroll-area";
 
 interface Scorer {
   scorers: {
@@ -19,8 +20,9 @@ interface Scorer {
 }
 
 function getOverallTopScorers(scorers: Scorer[]) {
-  for (const scorer of scorers) {
-  }
+  const allScorers = scorers.flatMap((scorer) => scorer.scorers);
+  allScorers.sort((a, b) => b.goals - a.goals);
+  return allScorers.slice(0, 30);
 }
 
 async function getTopScorersFromLeagues(nationalLeagues: [string, string][]) {
@@ -35,38 +37,116 @@ async function getTopScorersFromLeagues(nationalLeagues: [string, string][]) {
 }
 
 export default async function Players() {
-  const scorers = await getTopScorersFromLeagues(
+  const leagueScorers = await getTopScorersFromLeagues(
     Object.entries(nationalLeagues),
   );
-  const overallTopScorers = getOverallTopScorers(scorers);
+  const overallTopScorers = getOverallTopScorers(leagueScorers);
 
   return (
     <section>
       <h1 className="text-3xl font-semibold">Top Scorers</h1>
       <div className="mt-4 grid grid-cols-1 gap-8 lg:grid-cols-2">
-        {scorers.map((scorer) => {
+        <ScrollArea className="h-[380px] rounded-lg bg-background-300 p-6">
+          <h2 className="text-lg font-medium sm:text-xl">
+            Overall Top Scorers (Top 30)
+          </h2>
+          <table className="mt-2 w-full text-left">
+            <thead>
+              <tr className="border-b-2 border-foreground-400">
+                <th>Pos.</th>
+                <th>Player</th>
+                <th>Goals</th>
+                <th className="text-right">Assists</th>
+              </tr>
+            </thead>
+            <tbody>
+              {overallTopScorers.map((scorer, i) => {
+                return (
+                  <tr key={"Overall Top Scorer " + i}>
+                    <td>{i + 1}.</td>
+                    <td>
+                      <img
+                        src={scorer.team.crest}
+                        alt={scorer.team.name + " crest"}
+                        width={100}
+                        height={100}
+                        className="mr-1 inline w-4"
+                      />
+                      <span className="hidden sm:inline">
+                        {scorer.player.name}
+                      </span>
+                      <span className="inline sm:hidden">
+                        {(() => {
+                          const name = scorer.player.name;
+                          return name.indexOf(" ") === -1
+                            ? name
+                            : name.substring(name.indexOf(" "));
+                        })()}
+                      </span>
+                    </td>
+                    <td>{scorer.goals}</td>
+                    <td className="text-right">
+                      {scorer.assists > 0 ? scorer.assists : 0}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="flex flex-col"></div>
+        </ScrollArea>
+        {leagueScorers.map((league) => {
           return (
             <div
               className="flex flex-col rounded-lg bg-background-300 p-6"
-              key={"Top Scorers data for: " + scorer.leagueName}
+              key={"Top Scorers data for: " + league.leagueName}
             >
               <h2 className="text-lg font-medium sm:text-xl">
-                {scorer.leagueName}
+                {league.leagueName}
               </h2>
-              {scorer.scorers.map((scorer, i) => {
-                return (
-                  <span key={"League top scorer " + i}>
-                    {scorer.player.name} - {scorer.goals}{" "}
-                    <img
-                      src={scorer.team.crest}
-                      alt={scorer.team.name + " crest"}
-                      width={100}
-                      height={100}
-                      className="inline w-4"
-                    />
-                  </span>
-                );
-              })}
+              <table className="mt-2 w-full text-left">
+                <thead>
+                  <tr className="border-b-2 border-foreground-400">
+                    <th>Pos.</th>
+                    <th>Player</th>
+                    <th>Goals</th>
+                    <th className="text-right">Assists</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {league.scorers.map((scorer, i) => {
+                    return (
+                      <tr key={league.leagueName + " Top Scorer " + i}>
+                        <td>{i + 1}.</td>
+                        <td>
+                          <img
+                            src={scorer.team.crest}
+                            alt={scorer.team.name + " crest"}
+                            width={100}
+                            height={100}
+                            className="mr-1 inline w-4"
+                          />
+                          <span className="hidden sm:inline">
+                            {scorer.player.name}
+                          </span>
+                          <span className="inline sm:hidden">
+                            {(() => {
+                              const name = scorer.player.name;
+                              return name.indexOf(" ") === -1
+                                ? name
+                                : name.substring(name.indexOf(" "));
+                            })()}
+                          </span>
+                        </td>
+                        <td>{scorer.goals}</td>
+                        <td className="text-right">
+                          {scorer.assists > 0 ? scorer.assists : 0}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
             </div>
           );
         })}
